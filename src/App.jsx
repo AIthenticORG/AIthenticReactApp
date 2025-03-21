28162b902a39fb20e1fa30e2dece4a91ba233432
FlorisDrunen <floris.van.drunen@outlook.com>
2025-03-21 13:26:34 +0100

    van lokaal naar git verplaats

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <>
-     <Header/>
+      <Header/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
