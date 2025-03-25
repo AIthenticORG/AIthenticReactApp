faacd738702b5af74c8f8944cc7f0b9a344d6666
FlorisDrunen <floris.van.drunen@outlook.com>
2025-03-25 14:32:04 +0100

    fixed discover homepage text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,26 +8,27 @@ const HomePage = () => {
     return(
 
         <div className="relative w-full h-auto flex flex-col">
-            <div className="relative w-full h-[80vh]">
+            <div className="relative w-full h-[100vh]">
             <img
             src={HomePageImage}
             className="w-full h-full object-cover"
             alt="HomePageImage"
+            draggable="false"
             />
-            <div className="absolute top-1/4 left-30 text-white max-w-xl">
-            <h1 className="text-4xl font-bold">
-                <span className="text-white">Discover</span>, <span className="text-gray-300">use AI</span>,
-            </h1>
-            <h1 className="text-4xl font-bold mt-2">
-                <span className="text-white">Understand</span> <span className="text-gray-300">legal guidelines.</span>
-            </h1>
-            <p className="text-gray-300 mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam
-                voluptates sed beatae?
-            </p>
-            <button className="mt-6 px-6 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition duration-400">
-                Start learning
-            </button>
+            <div className="absolute top-1/3 ml-10 md:ml-20 lg:ml-40 text-white max-w-xxl">
+                <h1 className="text-7xl font-bold">
+                    <span className="text-white">Discover</span>, <span className="text-white">use AI</span>,
+                </h1>
+                <h1 className="text-6xl font-bold mt-2">
+                    <span className="text-white">Understand</span> <span className="text-white">legal guidelines.</span>
+                </h1>
+                <p className="text-gray-300 mt-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam
+                    voluptates sed beatae?
+                </p>
+                <button className="mt-6 px-6 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition duration-400">
+                    Start learning
+                </button>
             </div>
         </div>
         <StartLearningContainer/>
