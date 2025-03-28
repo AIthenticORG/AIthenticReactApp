cd24cd54d2bd6ccc78fe30a2686892c3db24b3c3
Bora <Bora@172-0-0-213.lightspeed.brhmal.sbcglobal.net>
2025-03-28 10:16:37 +0100

    bijwerken van translations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,12 @@ import ImageSlider from '../components/LogoSlider/ImageSlider';
 import InfoContainer from '../components/InfoContainer'
 import StartLearningContainer from '../components/StartLearningContainer';
 import AssociatedCompanies from '../components/AssosiatedCompanies';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
-    return(
+    const {t, i18n} = useTranslation();
 
+    return(
         <div className="relative w-full h-auto flex flex-col">
             <div className="relative w-full h-[100vh]">
             <img
@@ -17,10 +19,10 @@ const HomePage = () => {
             />
             <div className="absolute top-1/4 md:top-1/3 pl-10 mr-10 md:pl-20 lg:pl-40 text-white max-w-xxl">
                 <h1 className="text-[40px] md:text-[60px] font-bold">
-                    <span className="text-white">Discover</span>, <span className="text-white">use AI</span>,
+                    <span className="text-white">{t('Discover, use AI')}</span>,
                 </h1>
                 <h1 className="text-[40px] md:text-[60px] font-bold mt-2">
-                    <span className="text-white">Understand</span> <span className="text-white">legal guidelines.</span>
+                    <span className="text-white">{t('Understand legal guidelines')}</span>
                 </h1>
                 <p className="text-[22px] md:text-[30px] text-gray-300 mt-4">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam
