f2db61e5cfdea99340d4c847aaedc6d8247744c2
Jayro <Jayro@172-0-0-198.lightspeed.brhmal.sbcglobal.net>
2025-04-01 11:55:10 +0200

    subscriptionsSlider aangemaakt, content moet nog

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,42 @@
-import HomePageImage from '../assets/AchtergrondPNG.jpg';
-import ImageSlider from '../components/LogoSlider/ImageSlider';
-import InfoContainer from '../components/InfoContainer'
-import StartLearningContainer from '../components/StartLearningContainer';
-import AssociatedCompanies from '../components/AssosiatedCompanies';
+import HomePageImage from "../assets/AchtergrondPNG.jpg";
+import ImageSlider from "../components/LogoSlider/ImageSlider";
+import InfoContainer from "../components/InfoContainer";
+import StartLearningContainer from "../components/StartLearningContainer";
+import AssociatedCompanies from "../components/AssosiatedCompanies";
+import SubscriptionsSlider from "../components/SubscriptionsSlider/SubscriptionsSlider";
 
 const HomePage = () => {
-    return(
-
-        <div className="relative w-full h-auto flex flex-col">
-            <div className="relative w-full h-[100vh]">
-            <img
-            src={HomePageImage}
-            className="w-full h-full object-cover"
-            alt="HomePageImage"
-            draggable="false"
-            />
-            <div className="absolute top-1/4 md:top-1/3 pl-10 mr-10 md:pl-20 lg:pl-40 text-white max-w-xxl">
-                <h1 className="text-[40px] md:text-[60px] font-bold">
-                    <span className="text-white">Discover</span>, <span className="text-white">use AI</span>,
-                </h1>
-                <h1 className="text-[40px] md:text-[60px] font-bold mt-2">
-                    <span className="text-white">Understand</span> <span className="text-white">legal guidelines.</span>
-                </h1>
-                <p className="text-[22px] md:text-[30px] text-gray-300 mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam
-                    voluptates sed beatae?
-                </p>
-                <button className="mt-6 px-6 py-2 border border-white text-white rounded-full hover:bg-white hover:text-black transition duration-400">
-                    Start learning
-                </button>
-            </div>
+  return (
+    <div className="relative w-full h-auto flex flex-col items-center">
+      <div className="relative w-full h-screen">
+        <img
+          src={HomePageImage}
+          className="w-full h-full object-cover"
+          alt="HomePageImage"
+          draggable="false"
+        />
+        <div className="absolute top-1/4 sm:top-1/3 ml-6 sm:ml-10 md:ml-20 text-white max-w-screen-lg">
+          <h1 className="text-[30px] sm:text-[40px] md:text-[55px] font-bold leading-tight text-left">
+            <span className="text-white">Discover</span>, <span className="text-white">use AI</span>,
+          </h1>
+          <h1 className="text-[30px] sm:text-[40px] md:text-[55px] font-bold mt-2 leading-tight text-left">
+            <span className="text-white">Understand</span> <span className="text-white">legal guidelines.</span>
+          </h1>
+          <p className="text-[16px] sm:text-[22px] md:text-[26px] text-gray-300 mt-4 text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam voluptates sed beatae?
+          </p>
+          <button className="mt-6 px-6 py-3 text-[14px] sm:text-[16px] md:text-[18px] border border-white text-white rounded-full hover:bg-white hover:text-black transition duration-400">
+            Start learning
+          </button>
         </div>
-        <StartLearningContainer/>
-        <ImageSlider />
-        <InfoContainer />
-        <AssociatedCompanies />
-        </div>
+      </div>
+      <StartLearningContainer />
+      <SubscriptionsSlider />
+      <InfoContainer />
+      <ImageSlider />
+      <AssociatedCompanies />
+    </div>
+  );
+};
 
-    )
-}
-export default HomePage;  
+export default HomePage;
