f7d6684fae6f19f237dfa2845ea6bb50b2d52f40
Jayro <Jayro@172-0-0-198.lightspeed.brhmal.sbcglobal.net>
2025-04-02 14:25:33 +0200

    blog experts component fix

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -7,29 +7,29 @@ import voorbeeld3 from '../assets/voorbeeld3.jpeg';
 
 const InfoContainer = () => {
     return (
-        <div className="w-[100%] h-auto flex flex-col text-center bg-gradient-to-l items-center from-[#304B66] via-[#0D5B58] to-[#1BB1AB] p-[20px]">
+        <div className="w-full h-[auto] flex flex-col text-center bg-gradient-to-l items-center from-[#304B66] via-[#0D5B58] to-[#1BB1AB] p-[30px]">
             <h1 className="w-auto h-auto text-[#8CC3C0] text-[23px] mt-[5px]">Blog</h1>
             <h1 className="w-auto h-auto text-white font-bold text-[25px] mt-[5px]">What our experts are saying</h1>
-            <p className="w-full h-auto text-white text-[18px] mt-[5px]">Our Next Chapter in innovation, and combining communcation</p>
-            <div className="flex flex-wrap md:flex-column justify-evenly gap-[20px] items-center md:w-[780px] w-full h-full">
+            <p className="w-full h-auto text-white text-[18px] mt-[5px]">Our Next Chapter in innovation, and combining communication</p>
+            <div className="flex flex-wrap flex-col md:flex-row lg:justify-between gap-[40px] justify-center items-center w-full max-w-[1200px]">
 
-                <div className="w-[320px] h-[200px] flex flex-row items-center gap-[30px] text-center hover:scale-[1.05] transition-all duration-300 ease-in-out">
-                    <img draggable="false" className='select-none w-[150px] h-3/4 rounded-sm' src={fish} alt="" />
-                    <div className='flex flex-col text-center text-start'>
+                <div className="lg:w-[400px] h-[auto] flex flex-col sm:flex-row justify-center items-center gap-[10px] sm:gap-[30px] mt-[20px] text-center">
+                    <img draggable="false" className='select-none w-[200px] h-[200px] rounded-sm' src={fish} alt="" />
+                    <div className='flex flex-col text-center text-start w-[200px]'>
                         <h1 className='text-white font-bold text-[16px]'>Our Next Chapter</h1>
-                        <p className='text-white text-[12px] mt-[5px]'>Read more &gt;</p>
+                        <p className='text-white text-[12px] mt-[5px] hover:text-green-500 transition duration-400 ease-in-out cursor-pointer'>Read more &gt;</p>
                         <div className='flex flex-row items-center text-start'>
-                            <img draggable="false" className='select-none rounded-full w-[30px]' src={fish} alt="" />
+                            <img draggable="false" className='select-none rounded-full w-[30px]' src={voorbeeld1} alt="" />
                             <p className='text-white text-[12px] mt-[10px] ml-[10px]'>Bora Efe<br />19-03-2025</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-[320px] h-[200px] flex flex-row items-center gap-[30px] text-center hover:scale-[1.05] transition-all duration-300 ease-in-out">
-                    <img draggable="false" className='select-none w-[150px] h-3/4 rounded-sm' src={voorbeeld1} alt="" />
-                    <div className='flex flex-col text-center text-start'>
+                <div className="lg:w-[400px] h-[auto] flex flex-col sm:flex-row justify-center items-center gap-[10px] sm:gap-[30px] mt-[20px] text-center">
+                    <img draggable="false" className='select-none w-[200px] h-[200px] rounded-sm' src={voorbeeld1} alt="" />
+                    <div className='flex flex-col text-center text-start w-[200px]'>
                         <h1 className='text-white font-bold text-[16px]'>Our Next Mission</h1>
-                        <p className='text-white text-[12px] mt-[5px]'>Read more &gt;</p>
+                        <p className='text-white text-[12px] mt-[5px] hover:text-green-500 transition duration-400 ease-in-out cursor-pointer'>Read more &gt;</p>
                         <div className='flex flex-row items-center text-start'>
                             <img draggable="false" className='select-none rounded-full w-[30px]' src={fish} alt="" />
                             <p className='text-white text-[12px] mt-[10px] ml-[10px]'>Jayro Peters<br />19-03-2025</p>
@@ -37,23 +37,24 @@ const InfoContainer = () => {
                     </div>
                 </div>
 
-                <div className="w-[320px] h-[200px] flex flex-row items-center gap-[30px] text-center hover:scale-[1.05] transition-all duration-300 ease-in-out">
-                    <img draggable="false" className='select-none w-[150px] h-3/4 rounded-sm' src={voorbeeld2} alt="" />
-                    <div className='flex flex-col text-center text-start'>
+
+                <div className="lg:w-[400px] h-[auto] flex flex-col sm:flex-row justify-center items-center gap-[10px] sm:gap-[30px] mt-[20px] text-center">
+                    <img draggable="false" className='select-none w-[200px] h-[200px] rounded-sm' src={fish} alt="" />
+                    <div className='flex flex-col text-center text-start w-[200px]'>
                         <h1 className='text-white font-bold text-[16px]'>AIthentic Vision 2025</h1>
-                        <p className='text-white text-[12px] mt-[5px]'>Read more &gt;</p>
+                        <p className='text-white text-[12px] mt-[5px] hover:text-green-500 transition duration-400 ease-in-out cursor-pointer'>Read more &gt;</p>
                         <div className='flex flex-row items-center text-start'>
-                            <img draggable="false" className='select-none rounded-full w-[30px]' src={fish} alt="" />
+                            <img draggable="false" className='select-none rounded-full w-[30px]' src={voorbeeld1} alt="" />
                             <p className='text-white text-[12px] mt-[10px] ml-[10px]'>Frank Visser<br />19-03-2025</p>
                         </div>
                     </div>
-                </div>  
+                </div>
 
-                <div className="w-[320px] h-[200px] flex flex-row items-center gap-[30px] text-center hover:scale-[1.05] transition-all duration-300 ease-in-out">
-                    <img draggable="false" className='select-none w-[150px] h-3/4 rounded-sm' src={voorbeeld3} alt="" />
-                    <div className='flex flex-col text-center text-start'>
+                <div className="lg:w-[400px] h-[auto] flex flex-col sm:flex-row justify-center items-center gap-[10px] sm:gap-[30px] mt-[20px] text-center">
+                    <img draggable="false" className='select-none w-[200px] h-[200px] rounded-sm' src={voorbeeld1} alt="" />
+                    <div className='flex flex-col text-center text-start w-[200px]'>
                         <h1 className='text-white font-bold text-[16px]'>Our Next Flibus</h1>
-                        <p className='text-white text-[12px] mt-[5px]'>Read more &gt;</p>
+                        <p className='text-white text-[12px] mt-[5px] hover:text-green-500 transition duration-400 ease-in-out cursor-pointer'>Read more &gt;</p>
                         <div className='flex flex-row items-center text-start'>
                             <img draggable="false" className='select-none rounded-full w-[30px]' src={fish} alt="" />
                             <p className='text-white text-[12px] mt-[10px] ml-[10px]'>Bill Clinton<br />19-03-2025</p>
@@ -63,7 +64,7 @@ const InfoContainer = () => {
 
             </div>
         </div>
-        
     );
-    }
+};
+
 export default InfoContainer;
