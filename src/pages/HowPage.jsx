994473880bb75d4f15a96a1819797f1b30034ee5
Jayro <jayro@172-0-0-181.lightspeed.brhmal.sbcglobal.net>
2025-04-29 09:03:43 +0200

    howpage start

--- src/pages/HowPage.jsx
+++ src/pages/HowPage.jsx
@@ -4,7 +4,7 @@ import hoe3 from '../assets/hoe3.png';
 
 const HowPage = () => {
     return(
-        <div className="w-full min-h-[auto] flex flex-col items-center pt-20 md:pt-28">
+        <div className="w-full flex flex-col items-center pt-20 md:pt-28">
             <div className="w-[80%] max-w-7xl flex flex-col gap-16">
                 <div className="flex flex-col md:flex-row items-center gap-12">
                     <div className="w-full lg:w-1/2 flex flex-col gap-6">
@@ -15,16 +15,16 @@ const HowPage = () => {
                         <br />
                         <h1 className='text-2xl md:text-3xl font-semibold'>Algemeen</h1>
                         <p>AIthentic biedt end-to-end AI-adviesdiensten op maat voor organisaties in de gezondheidszorg, waaronder strategieontwikkeling, samenwerking tussen teams en AI, AI-implementaties en voortdurende optimalisatie als gevolg van de snelle veranderingen. Het bijhouden van de ontwikkelingen van (exponentiële) technologieën is niet het primaire aandachtsgebied van zorginstellingen, maar kan door gespecialiseerde bedrijven worden aangeboden.</p>
-                        <l>
+                        <ul className='list-disc list-inside text-sm md:text-base leading-relaxed'>
                             <li>
-                                
+                            We laten onze diensten voor een groot deel leiden door een assessment die we uitvoeren binnen uw organisatie. Door middel van interviews, aangevuld met andere vormen van gegevensverzameling, zijn we in staat om een AI-vingerafdruk te maken van vrijwel elk type organisatie.
                             </li>
                             <li>
-
+                                Onze aanpak is eenvoudig en holistisch. Het domein verandert zo snel dat het beter is te kijken naar de mogelijkheden, uitdagingen en consequenties. Ons wereldwijde netwerk van mensen die zich bezighouden met AI zijn onze bakens om zonder kleerscheuren te navigeren en voortdurend kritisch te zijn en de juiste vragen te stellen.
                             </li>
-                        </l>
+                        </ul>
                     </div>
-    
+
                     <div className="hidden lg:flex w-full lg:w-1/2 justify-center items-center">
                         <div className="w-full h-full flex justify-center items-center p-5 rounded-lg">
                             <img src={hoe} alt="Waarom" draggable="false" className="w-[260px] md:w-[400px] lg:w-[500px] h-auto object-contain select-none"/>
@@ -40,7 +40,7 @@ const HowPage = () => {
                             <img src={hoe2} alt="Waarom" draggable="false" className="w-[260px] md:w-[400px] lg:w-[500px] h-auto object-contain select-none"/>
                         </div>
                     </div>
-                    <div className="w-full lg:w-1/2 flex flex-col gap-6 mt-[0px] md:mt-[50px]">
+                    <div className="w-full lg:w-1/2 flex flex-col gap-6 mt-0 md:mt-[50px]">
                         <p className="text-sm md:text-base leading-relaxed">
                             De indrukwekkende kanten van AI brengen ook veel onzekerheden met zich mee.
                         </p>
